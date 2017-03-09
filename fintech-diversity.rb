478c7399a107923c7d7bbc05ebe2cb1c149e6cfe
katherinebui <kathy.codes@gmail.com>
2017-03-09 11:31:28 -0800

    import companies in csv file

--- fintech-diversity.rb
+++ fintech-diversity.rb
@@ -1,7 +1,10 @@
 require 'csv'
 
+# goal: fintech, london, tech teams < 15% female
 csv_text = File.read('companies.csv')
 csv = CSV.parse(csv_text, headers: true)
 csv.each do |row|
   p row.to_h
 end
+
+
