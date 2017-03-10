a3b58d56625048a46fd7c9d4062d0b2e810e3923
katherinebui <kathy.codes@gmail.com>
2017-03-09 20:40:31 -0800

    filter complete

--- fintech-diversity.rb
+++ fintech-diversity.rb
@@ -1,4 +1,5 @@
 require 'csv'
+require 'pp'
 
 # goal: fintech, london, tech teams < 15% female
 csv = CSV.read('companies.csv', headers: true, header_converters: [:symbol, :downcase])
@@ -7,17 +8,16 @@ array_companies = csv.map{|row| row.to_h}
 array_companies.map do |company|
   company[:percent_female_eng] = company[:percent_female_eng].to_i
   company[:industry] = company[:industry].downcase
+  company[:location] = company[:location].downcase
 end
 
-p array_companies
+# p array_companies
 
-# filter = []
-# array_companies.each do |company|
-#   if company[:location] == "london" && company[:industry] == "fintech" && company[:]
-#     filter << company
-#   end
-# end
+filter = []
+array_companies.each do |company|
+  if company[:location] == "london" && company[:industry] == "fintech" && company[:percent_female_eng] < 15
+    filter << company
+  end
+end
 
-
-# p filter
-# p filter.length
+pp filter
